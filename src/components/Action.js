a785002f4ec9d2931149e27c3d4c5ce33f189446
Stackbit Code Editor <projects@stackbit.com>
2021-09-14 20:42:22 +0000

    Action.js: changed

--- src/components/Action.js
+++ src/components/Action.js
@@ -19,7 +19,7 @@ export default class Action extends React.Component {
                 <span className="screen-reader-text">{_.get(action, 'label', null)}</span>
               </React.Fragment>) : (
                 <> _.get(action, 'prefix', null)
-                </>
+               
                 )
               }
             </Link>
